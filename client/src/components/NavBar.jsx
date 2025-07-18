db96abdff8129540302e7bf23b957c85622833d2
vatsal <vatsal.vns@gmail.com>
2025-07-18 23:54:43 +0530

    Initial project setup

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { useClerk,UserButton,useUser } from '@clerk/clerk-react'
+
+const NavBar = () => {
+      const {openSignIn} = useClerk()
+     
+
+  return (
+    <div className = 'shadow py-4'>
+        <div className = 'container px-4 2xl:px-2 mx-auto flex justify-between items-center'>
+           <img src = {assets.logo} alt = ""/>
+        <div className = 'flex gap-4 max-sm:text-2xl'>
+            <button className = 'text-gray-600'>Recruiter Login </button>
+            <button onClick={e => openSignIn()} className = 'bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
