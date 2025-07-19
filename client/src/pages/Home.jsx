60d30b30d4516178563f6d3258221f90365ee2a9
vatsal.vns@gmail.com <vatsal.vns@gmail.com>
2025-07-19 23:50:08 +0530

    modified navbar and added  clerk

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import Hero from '../components/Hero'
  
 
 function Home() {
   return (
     <div>
         <NavBar/>
+        <Hero/>
     </div>
   )
 }
