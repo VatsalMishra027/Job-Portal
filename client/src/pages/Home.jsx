0edf08ab8cdf1ae1b59ae1d89f69a4a1c819dcdc
vatsal.vns@gmail.com <vatsal.vns@gmail.com>
2025-07-20 23:56:23 +0530

    Added filter and pagination section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Hero from '../components/Hero'
+import JobListing from '../components/JobListing'
  
 
 function Home() {
@@ -8,6 +9,8 @@ function Home() {
     <div>
         <NavBar/>
         <Hero/>
+        <JobListing/>
+        
     </div>
   )
 }
