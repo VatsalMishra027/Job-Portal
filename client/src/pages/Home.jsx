2c39f5132b446cad55f6664b923f844f44b3b9ea
vatsal.vns@gmail.com <vatsal.vns@gmail.com>
2025-07-21 23:54:10 +0530

    Added footer and app download section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import Hero from '../components/Hero'
 import JobListing from '../components/JobListing'
+import AppDownload from '../components/AppDownload'
+import Footer from '../components/Footer'
  
 
 function Home() {
@@ -10,6 +12,8 @@ function Home() {
         <NavBar/>
         <Hero/>
         <JobListing/>
+        <AppDownload/>
+        <Footer/>
         
     </div>
   )
