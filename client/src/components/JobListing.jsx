e09051d6e341f038acd26b17a9493d582b838dc4
vatsal.vns@gmail.com <vatsal.vns@gmail.com>
2025-07-23 23:59:02 +0530

    Updated apply page and application page

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -120,7 +120,7 @@ const JobListing = () => {
                         <img onClick={() =>setCurrentPage(Math.max(currentPage-1,1))} src={assets.left_arrow_icon} alt="" />
                     </a>
                     {Array.from({ length: Math.ceil(filteredJobs.length / 6) }).map((_, index) => (
-                      <a href="#jobs-list" key={index}>
+                      <a  href="#jobs-list" key={index}>
                         <button onClick={() => setCurrentPage(index+1)} className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded ${currentPage === index + 1 ? 'bg-blue-100 text-blue-500' : 'text-gray-500'}`}>{index + 1} </button>
                          </a>
                           ))}
--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 function Applications() {
+  const[isEdit,setIsEdit] = useState(false)
+  const[resume,setResume] = useState()
   return (
-    <div>Applications</div>
+     <>
+     <NavBar/>
+     <div className ='container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10'>
+      <h2 className='text-xl font-semibold' >Your Resume</h2>
+      <div className = 'flex gap-2 mb-6 mt-3' >
+      {
+        isEdit ? 
+        <>
+         <label htmlFor=''>
+           <p>Select Resume</p>
+           <input accept ='application/pdf' type="text" />
+         </label>
+        </>
+        : <div className='flex gap-2' >
+          <a className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg' href="">
+            Resume
+          </a>
+          <button onClick={() => setIsEdit(true)} className='text-gray-500 border border-gray-300 rounded-lg px-4 py-2'>
+            Edit
+          </button>
+           </div>
+      }
+     
+     </div>
+     </div>
+     </>
   )
 }
 
